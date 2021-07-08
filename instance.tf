ba4c5577e6ce4cd44c44866acddb0a541c2d1f12
Denis Boulas <dene14@gmail.com>
2021-07-08 18:34:18 +0300

    Handled map() deprecation (tf-0.12)

--- instance.tf
+++ instance.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "bastion" {
   iam_instance_profile   = "${aws_iam_instance_profile.bastion.name}"
   vpc_security_group_ids = ["${aws_security_group.bastion.id}"]
   user_data              = "${data.template_file.user_data.rendered}"
-  tags                   = "${merge(map("Name", "${var.environment}-${var.name}"),"${var.tags}")}"
+  tags                   = merge({"Name" = "${var.environment}-${var.name}"}, var.tags)
 
   # Any metadata changes will trigger instance destroy
   # see: https://github.com/terraform-providers/terraform-provider-aws/issues/23
